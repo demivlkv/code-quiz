c04e1137b6462292643be745aefbabfb5ac25542
Demi Hayashi <demi.h@me.com>
2022-06-27 20:26:06 -0700

    add initial javascript

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+let startButton = document.getElementById('start-btn');
+startButton.addEventListener('click', startGame);
+
+// begin the quiz upon button click
+function startGame() {
+    console.log('Quiz start!');
+}
+
+// initiate the next question
+
+function nextQuestion() {
+
+}
+
+// answering a question
+
+function chooseAnswer() {
+
+}
